430f678d990fa904746078ac255f74d99be4ec56
Ulrik <ulrik.strid@outlook.com>
2019-07-28 21:07:56 +0200

    Add build for frontend
    Co-authored-by: Simon Gottschlag <simon.gottschlag@xenit.se>

--- src/backend-cart/src/index.ts
+++ src/backend-cart/src/index.ts
@@ -10,8 +10,8 @@ app.get("/healthz", (req, res) => {
 
 app.use(expressLogger);
 
-app.get("/api/v1/cart/echo", (req, res) => {
-  res.send("echo");
+app.get("/api/v1/cart/:id", (req, res) => {
+  res.json({ id: req.params.id }).send();
 });
 
 app.listen(port, () => console.log(`Cart service listening on port ${port}`));
--- src/frontend-shop/src/index.tsx
+++ src/frontend-shop/src/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+fetch("/api/v1/cart/echo")
+  .then(res => res.json())
+  .then(cart => console.log(cart));
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
